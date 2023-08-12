7b8701bc65b010bfda3314af3ec49c5e517f0576
Harshvardhan <harshv@anisoft.us>
2023-08-12 16:30:29 +0530

    Switch updated with BLoc

--- lib/bloc/upload_bloc.dart
+++ lib/bloc/upload_bloc.dart
@@ -1,14 +1,13 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:upload_form/bloc/upload_event.dart';
 import 'package:upload_form/bloc/upload_state.dart';
 
-class contentBloc extends Bloc<ContentEvent, ContentState> {
-  contentBloc() : super(UploadInitState()) {
+class contentBloc extends Bloc<ContentEvent, bool> {
+  contentBloc() : super(false) {
     on<UploadAllowDownloadEvent>((event, emit) {
       // Toggling the switch value
       event.switchValue = !event.switchValue;
+      emit(event.switchValue);
     });
     // on((event, emit) => null);
   }
--- lib/upload_form.dart
+++ lib/upload_form.dart
@@ -1,15 +1,9 @@
-import 'dart:io';
-
 import 'package:dotted_border/dotted_border.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:upload_form/bloc/upload_bloc.dart';
 import 'package:upload_form/bloc/upload_event.dart';
-import 'package:upload_form/bloc/upload_state.dart';
 import 'package:upload_form/constants.dart';
 import 'package:upload_form/services/upload.service.dart';
 import 'package:upload_form/styles.dart';
@@ -23,9 +17,11 @@ class UploadScreen extends StatefulWidget {
 
 class _UploadScreenState extends State<UploadScreen> {
   final HtmlEditorController _htmlEditorController = HtmlEditorController();
-  bool _allowDownload = false;
+
   @override
   Widget build(BuildContext context) {
+    bool allowedDownload = context.watch<contentBloc>().state;
+
     return Scaffold(
       appBar: AppBar(title: const Text("Content")),
       body: SingleChildScrollView(
@@ -62,10 +58,6 @@ class _UploadScreenState extends State<UploadScreen> {
             const SizedBox(
               height: kPadding,
             ),
-            // const Text(
-            //   "Content Upload",
-            //   style: titleStyle,
-            // ),
             RichText(
                 text: const TextSpan(
                     text: "Content Upload",
@@ -124,17 +116,17 @@ class _UploadScreenState extends State<UploadScreen> {
                   "Allow user to download content",
                   style: titleStyle,
                 ),
-                Spacer(),
-                // BlocBuilder<contentBloc, ContentState>(
-                //   builder: (context, state) {
-                //     return Switch(
-                //         value:state,
-                //         onChanged: (value) {
-                //           BlocProvider.of<contentBloc>(context)
-                //               .add(UploadAllowDownloadEvent(_allowDownload));
-                //         });
-                //   },
-                // )
+                const Spacer(),
+                BlocBuilder<contentBloc, bool>(
+                  builder: (context, state) {
+                    return Switch(
+                        value: allowedDownload,
+                        onChanged: (value) {
+                          BlocProvider.of<contentBloc>(context)
+                              .add(UploadAllowDownloadEvent(state));
+                        });
+                  },
+                )
               ],
             ),
           ]),
